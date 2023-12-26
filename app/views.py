3674d415d9108254930b5c32e533c54ed2e02a7b
niteesh <a.nithishkumarreddy922@gmail.com>
2023-12-26 12:26:04 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def niteesh(request):
+    return render(request,'niteesh.html')
+
+
+def harish(request):
+    return render(request,'harish.html')
